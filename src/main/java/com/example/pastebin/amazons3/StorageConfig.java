411151c2591a491292091ab23bbee095ed836791
Atom <pvana7065@gmail.com>
2023-08-21 18:39:15 +0300

    поменял зависимость для amazon s3 и переписал файл конфиграции для s3 и его сервис для работы с ним

--- src/main/java/com/example/pastebin/amazons3/StorageConfig.java
+++ src/main/java/com/example/pastebin/amazons3/StorageConfig.java
@@ -4,44 +4,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-
 import lombok.Data;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Data
 @Configuration
 public class StorageConfig {
 	
-	@Value("${cloud.aws.credentials.access-key}")
-	private String accessKey;
-	
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String secretKey;
-	
-	@Value("${cloud.aws.region.static}")
+	@Value("${aws.region}")
 	private String region;
 	
 	@Value("${application.bucket.name}")
 	private String bucketName;
 	
 	@Bean
-	public AmazonS3 createS3Client() {
-		return AmazonS3ClientBuilder
-					.standard()
-					.withCredentials(
-							new AWSStaticCredentialsProvider(getCredentials()))
-					.withRegion(region)
-					.build();
+	public S3Client createS3Client() {
+		return S3Client.builder()
+	                .region(Region.of(region))
+	                .build();
 	}
 	
-	private AWSCredentials getCredentials() {
-        return new BasicAWSCredentials(
-                accessKey,
-                secretKey
-        );
-    }
 }
--- src/main/java/com/example/pastebin/services/AmazonS3ClientService.java
+++ src/main/java/com/example/pastebin/services/AmazonS3ClientService.java
@@ -1,26 +1,57 @@
 package com.example.pastebin.services;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.example.pastebin.amazons3.StorageConfig;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 public class AmazonS3ClientService {
 	
-	private AmazonS3 s3Client;
-	private StorageConfig config;
+	@Value("${application.bucket.name}")
+	private String bucketName;
 	
-	public AmazonS3ClientService(AmazonS3 s3Client, StorageConfig config) {
-		this.s3Client = s3Client;
-		this.config = config;
+	private S3Client s3;
+	
+	public AmazonS3ClientService(S3Client s3) {
+		this.s3 = s3;
 	}
 	
 	public void putObject(String objectName, String text) {
-		s3Client.putObject(config.getBucketName(), objectName, text);
+		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectName)
+                .build();
+		
+		s3.putObject(putObjectRequest, RequestBody.fromBytes(text.getBytes()));
+		
+	}
+	
+	public String getObject(String objectName) {
+		GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectName)
+                .build();
+		var response = s3.getObject(getObjectRequest);
+		try {
+			return new String(response.readAllBytes());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	public void deleteObject(String objectName) {
-		s3Client.deleteObject(config.getBucketName(), objectName);
+		DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectName)
+                .build();
+
+        s3.deleteObject(deleteObjectRequest);
 	}
 }
